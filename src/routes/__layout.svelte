432d4a5bcdf8dab5aa80bf9bda0224bb8ff1b6e4
Hayden Mankin <DryCreations@gmail.com>
2021-05-15 16:58:24 -0400

    base url for gh-pages

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import {base} from '$app/paths';
+</script>
+
+<svelte:head>
+    <link rel="icon" href={`${base}/favicon.ico`} />
+</svelte:head>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,8 @@
+<script>
+    import {base} from '$app/paths';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<a href="/example/stuff">Navigate to subfolder</a>
+<a href="{base}/example/stuff">Navigate to subfolder</a>
